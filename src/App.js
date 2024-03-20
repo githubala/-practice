86c6438db8753cecee87dab9fd52f11dfacd5733
githubala <balachandra.kun@gmail.com>
2024-03-20 16:46:17 +0530

    changes for the container component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import './style.css';
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import UserDetails from './components/UserDetails';
-import Notfound from './components/Notfound';
+import Header from './components/Header'
+
+
 export default function App() {
-  let id = '123';
+  
   return (
-    <div className='AppClr'>
-      <BrowserRouter>
-        <Link to="/">Home</Link>
-        <Link to={`/User/${id}`}>User</Link>
-        <Routes>
-          <Route path="/" Component={Home}></Route>
-          <Route exact path="/User/:id" Component={UserDetails}></Route>
-          <Route path="*" Component={Notfound}></Route>
-        </Routes>
-      </BrowserRouter>
+    <div>
+      <Header></Header>
     </div>
   );
 }
